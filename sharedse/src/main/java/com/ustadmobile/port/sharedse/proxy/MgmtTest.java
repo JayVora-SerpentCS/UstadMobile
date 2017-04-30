c4e8cfdea1a535d36450146ac346841f1078f796
Varuna Singh <varuna.singh@gmail.com>
2017-04-30 16:33:58 +0400

    Added gwt module Added ustadmobile-entitygen module Added a test entity to sharedse that gets built to ustadmobile-entitygen

---
+++ sharedse/src/main/java/com/ustadmobile/port/sharedse/proxy/MgmtTest.java
@@ -0,0 +1,21 @@
+package com.ustadmobile.port.sharedse.proxy;
+
+/**
+ * Created by Varuna on 4/27/2017.
+ */
+public interface MgmtTest {
+
+    /**
+     * @nanolrs.primarykey
+     *
+     * @return
+     */
+    String getTestId();
+
+    void setTestId(String testId);
+
+    String getCanonicalData();
+
+    void setCanonicalData(String canonicalData);
+
+}
